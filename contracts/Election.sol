1927e52258fd8549c2ceb3f68b863f98eb917a2e
Jon Rylatt <jonathon_rylatt@yahoo.com>
2022-03-29 14:49:03 +0800

    Voting and double checking done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -16,6 +16,8 @@ contract Election {
   }
   //Fetch candidate
   mapping(uint => Candidate) public candidates;
+  //Account storage
+  mapping(address =>bool) public voters;
   //candidate count
   uint public candidatesCount;
   constructor () public {
@@ -29,4 +31,14 @@ contract Election {
   /*Ask candidate name (double check name with a=1)Get votes for new candidate, count current candidate votes, if votes>threshold, 
   call add Candidate with name, random id, and vote count of 0*/ 
 
+  function vote (uint _candidateId) public{
+    //Make sure no multiple votes
+    require(!voters[msg.sender]);
+    //Vote for valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+    //keep track of voter votes (no multiple)
+    voters[msg.sender] = true;
+    //update votes (Update with weighted votes based on held tokens)
+    candidates[_candidateId].voteCount++;
+  }
 }
